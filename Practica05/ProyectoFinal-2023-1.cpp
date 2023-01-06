5dc86a7dda99124db74b6e48128daa4b9d1e5f3e
sorek7575 <89646840+sorek75@users.noreply.github.com>
2023-01-06 08:55:07 -0600

    add muro

--- Practica05/ProyectoFinal-2023-1.cpp
+++ Practica05/ProyectoFinal-2023-1.cpp
@@ -1012,7 +1012,7 @@ int main()
 		// reja
 		// ------------------------------------------------------------------------------------------------------------------------- 
 		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(10.0f));
+		model = glm::scale(model, glm::vec3(1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
